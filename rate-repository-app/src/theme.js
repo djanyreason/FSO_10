b5d0016035e0b07fb3c968f80377f767c237699d
John Anderson <djanyreason@gmail.com>
2023-12-14 15:18:46 -0500

    Completes ex 10.10, adds platform-specific fonts
    This commit uses Platform.select to implement platform-specific fonts

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
 	colors: {
 		textPrimary: '#24292e',
@@ -12,7 +14,11 @@ const theme = {
 		subheading: 16,
 	},
 	fonts: {
-		main: 'System',
+		main: Platform.select({
+			android: 'Roboto',
+			iOS: 'Arial',
+			default: 'System',
+		}),
 	},
 	fontWeights: {
 		normal: '400',
