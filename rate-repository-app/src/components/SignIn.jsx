36574960cc4f402f071305acf9b21c93824683de
John Anderson <djanyreason@gmail.com>
2023-12-14 14:49:12 -0500

    Completes ex 10.8, adds sign-in form via Formik
    This commit formats a sign in page input using the formik library for
    text input. The sign in form currently only logs the username and
    password to console.

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,75 @@
+import { Pressable, View, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+
 import Text from './Text';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+	form: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'stretch',
+		backgroundColor: 'white',
+		padding: 15,
+	},
+	input: {
+		marginBottom: 15,
+		borderWidth: 1,
+		borderColor: 'black',
+		borderRadius: 2,
+		padding: 7,
+	},
+	button: {
+		borderRadius: 2,
+		backgroundColor: theme.colors.primary,
+		display: 'flex',
+		alignItems: 'center',
+		padding: 9,
+	},
+	text: {
+		color: 'white',
+	},
+});
+
+const initialValues = {
+	login: '',
+	password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+	return (
+		<View style={styles.form}>
+			<FormikTextInput
+				name='login'
+				placeholder='Username'
+				style={styles.input}
+			/>
+			<FormikTextInput
+				name='password'
+				placeholder='Password'
+				style={styles.input}
+				secureTextEntry
+			/>
+			<Pressable onPress={onSubmit} style={styles.button}>
+				<Text style={styles.text} fontWeight={'bold'}>
+					Sign In
+				</Text>
+			</Pressable>
+		</View>
+	);
+};
 
 const SignIn = () => {
-	return <Text>The sign-in view</Text>;
+	const onSubmit = (values) => {
+		console.log(values);
+	};
+
+	return (
+		<Formik initialValues={initialValues} onSubmit={onSubmit}>
+			{({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+		</Formik>
+	);
 };
 
 export default SignIn;
